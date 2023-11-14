74dad36fb98142ca822cf809be3faf969443c51c
IainMosima <iainmosima@gmail.com>
2023-11-14 23:49:06 +0300

    added colors and fonts

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -10,13 +10,17 @@ const config: Config = {
     extend: {
       colors: {
         green: '#27A949',
+        darkGreen: '#3B7A4C',
         orange: '#EC910F',
+        darkOrange: '#CF7C06',
         grey: '#5E5E5E',
         lightGrey: '#707070',
         lightWhite: '#FDFDFD'
       },
       fontFamily: {
-        
+        base: ['PT Sans', 'sans-serif'],
+        manrope: ['Manrope', 'sans-serif'],
+        space: ['Space Grotesk', 'sans-serif']
       }
     },
   },
