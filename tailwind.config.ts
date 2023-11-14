f221501a75ae6139a421a0a8ab9096a570d25044
IainMosima <iainmosima@gmail.com>
2023-11-14 23:44:26 +0300

    setting up the project

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -8,11 +8,16 @@ const config: Config = {
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+      colors: {
+        green: '#27A949',
+        orange: '#EC910F',
+        grey: '#5E5E5E',
+        lightGrey: '#707070',
+        lightWhite: '#FDFDFD'
       },
+      fontFamily: {
+        
+      }
     },
   },
   plugins: [],
